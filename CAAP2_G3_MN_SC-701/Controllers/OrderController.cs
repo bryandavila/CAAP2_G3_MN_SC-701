eff217fe688c824205750306284ce9c0b157d5b8
bryandavila <badmdavila@gmail.com>
2025-04-09 19:28:54 -0600

    Se agrega el controlador

---
+++ CAAP2_G3_MN_SC-701/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CAAP2.Services.Services;
+
+namespace CAAP2_G3_MN_SC_701.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _orderService.GetAllOrdersAsync();
+            return View(orders);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var order = await _orderService.GetOrderByIdAsync(id);
+            if (order == null)
+                return NotFound();
+
+            return View(order);
+        }
+    }
+}
