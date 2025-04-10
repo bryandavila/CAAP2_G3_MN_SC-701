381ac72586720e2d3b342c2d6b41b96d85d75539
bryandavila <badmdavila@gmail.com>
2025-04-09 19:05:05 -0600

    Actualizacion del program.cs

--- CAAP2_G3_MN_SC-701/Program.cs
+++ CAAP2_G3_MN_SC-701/Program.cs
@@ -1,23 +1,38 @@
+using CAAP2.Data.MSSQL.OrdersDB;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuración de DbContext
+builder.Services.AddDbContext<OrdersDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OrdersConnection")));
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Agrega servicios de controllers y vistas (para MVC)
+builder.Services.AddControllersWithViews();
+
+// Configurar Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+// Rutas por defecto
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
