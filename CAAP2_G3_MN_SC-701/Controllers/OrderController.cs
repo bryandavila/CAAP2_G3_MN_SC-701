07d2ed290c39638f07ae10c982cdf544eb94559e
bryandavila <badmdavila@gmail.com>
2025-04-09 19:45:21 -0600

    Se agregan vistas create, delete, edit

--- CAAP2_G3_MN_SC-701/Controllers/OrderController.cs
+++ CAAP2_G3_MN_SC-701/Controllers/OrderController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CAAP2.Models;
 using CAAP2.Services.Services;
+using CAAP2.Data.MSSQL.OrdersDB;
+using Microsoft.EntityFrameworkCore;
 
 namespace CAAP2_G3_MN_SC_701.Controllers
 {
     public class OrderController : Controller
     {
         private readonly IOrderService _orderService;
+        private readonly OrdersDbContext _context;
 
-        public OrderController(IOrderService orderService)
+        public OrderController(IOrderService orderService, OrdersDbContext context)
         {
             _orderService = orderService;
+            _context = context;
         }
 
         [HttpGet]
@@ -27,6 +33,74 @@ namespace CAAP2_G3_MN_SC_701.Controllers
                 return NotFound();
 
             return View(order);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["UserID"] = new SelectList(_context.Users, "UserID", "FullName");
+            ViewData["OrderTypeId"] = new SelectList(_context.OrderTypes, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                await _orderService.CreateOrderAsync(order);
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["UserID"] = new SelectList(_context.Users, "UserID", "FullName", order.UserID);
+            ViewData["OrderTypeId"] = new SelectList(_context.OrderTypes, "Id", "Name", order.OrderTypeId);
+            return View(order);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var order = await _orderService.GetOrderByIdAsync(id);
+            if (order == null)
+                return NotFound();
+
+            ViewData["UserID"] = new SelectList(_context.Users, "UserID", "FullName", order.UserID);
+            ViewData["OrderTypeId"] = new SelectList(_context.OrderTypes, "Id", "Name", order.OrderTypeId);
+            return View(order);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                await _orderService.UpdateOrderAsync(order);
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["UserID"] = new SelectList(_context.Users, "UserID", "FullName", order.UserID);
+            ViewData["OrderTypeId"] = new SelectList(_context.OrderTypes, "Id", "Name", order.OrderTypeId);
+            return View(order);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var order = await _orderService.GetOrderByIdAsync(id);
+            if (order == null)
+                return NotFound();
+
+            return View(order);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _orderService.DeleteOrderAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
